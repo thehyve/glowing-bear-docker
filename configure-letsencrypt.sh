e2a00e8fcb954c6fbb93e24e8f6375cea9e55226
Gijs Kant <gijs@thehyve.nl>
2019-10-04 18:14:32 +0200

    Add host header to proxy to fix download from notebook server.

--- configure-letsencrypt.sh
+++ configure-letsencrypt.sh
@@ -43,6 +43,7 @@ server {
     proxy_set_header      X-Forwarded-For \$proxy_add_x_forwarded_for;
     proxy_set_header      X-Forwarded-Proto \$scheme;
     proxy_set_header      Proxy "";
+    proxy_set_header      Host \$host;
     proxy_http_version    1.1;
     proxy_set_header      Upgrade \$http_upgrade;
     proxy_set_header      Connection "Upgrade";

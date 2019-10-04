c8680a6fbd778ae599979dcc5925f0e4afd47e6c
Gijs Kant <gijs@thehyve.nl>
2019-10-04 11:43:02 +0200

    Fix CORS for the notebook server.

--- docker/tmtk-notebook-server/docker-entrypoint.sh
+++ docker/tmtk-notebook-server/docker-entrypoint.sh
@@ -4,4 +4,4 @@ set -e
 
 password_hash=$(python -c "from notebook.auth import passwd; print(passwd('${TMTK_NOTEBOOK_PASSWORD}'))")
 
-exec jupyter notebook --NotebookApp.password="${password_hash}" --no-browser --ip 0.0.0.0
+exec jupyter notebook --NotebookApp.allow_origin='*' --NotebookApp.password="${password_hash}" --no-browser --ip 0.0.0.0
